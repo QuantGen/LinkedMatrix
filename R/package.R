a22327f4f634409b8901d85d11873aa94e7f3544
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2015-10-06 18:05:54 -0400

    Properly document package.

--- R/package.R
+++ R/package.R
@@ -1,5 +1,7 @@
 #' LinkedMatrix: Column-Linked and Row-Linked Matrices
-#'
+#' 
+#' Matrices implemented as collections of matrix-like nodes, linked by columns or rows.
+#' 
 #' @docType package
 #' @name LinkedMatrix
 #' @import methods
