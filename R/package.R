54a5b5aa42c86587939c942de2790ee0772e6b5d
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2015-11-02 18:02:00 -0500

    Print NIH grant numbers when attaching the package.

--- R/package.R
+++ R/package.R
@@ -6,3 +6,8 @@
 #' @name LinkedMatrix
 #' @import methods
 NULL
+
+
+.onAttach <- function(libname, pkgname) {
+    packageStartupMessage("Support provided by the National Institutes of Health (Grant: R01GM101219, R01GM099992).")
+}
