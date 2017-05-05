3b04fcd047b0b2cbcc7c1116c478e0a07da573bc
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2017-05-05 06:22:36 -0400

    Test if suggested packages are installed

--- man/examples/LinkedMatrix-class.R
+++ man/examples/LinkedMatrix-class.R
@@ -1,10 +1,20 @@
 # Create an example RowLinkedMatrix from various matrix-like objects that
 # correspond in dimensions
-m <- RowLinkedMatrix(
-    ff::ff(initdata = rnorm(50), dim = c(5, 10)),
-    bigmemory::big.matrix(init = rnorm(50), nrow = 5, ncol = 10),
-    matrix(data = rnorm(50), nrow = 5, ncol = 10)
-)
+if (require("ff")) {
+    m1 <- ff::ff(initdata = rnorm(50), dim = c(5, 10))
+} else {
+    m1 <- matrix(data = rnorm(50), nrow = 5, ncol = 10)
+}
+
+if (require("bigmemory")) {
+    m2 <- bigmemory::big.matrix(init = rnorm(50), nrow = 5, ncol = 10)
+} else {
+    m2 <- matrix(data = rnorm(50), nrow = 5, ncol = 10)
+}
+
+m3 <- matrix(data = rnorm(50), nrow = 5, ncol = 10)
+
+m <- RowLinkedMatrix(m1, m2, m3)
 
 # Test if m is an object of either type ColumnLinkedMatrix or RowLinkedMatrix
 if (is(m, "LinkedMatrix")) {
--- man/examples/LinkedMatrix.R
+++ man/examples/LinkedMatrix.R
@@ -6,17 +6,21 @@ nNodes(m1)
 all(sapply(m1, class) == "matrix")
 
 # Create an empty 15x10 RowLinkedMatrix with 3 ff nodes
-m2 <- LinkedMatrix(nrow = 15, ncol = 10, nNodes = 3, linkedBy = "rows",
-                   nodeInitializer = "ffNodeInitializer", vmode = "byte")
-dim(m2)
-nNodes(m2)
-all(sapply(m2, inherits, "ff_matrix"))
+if (require("ff")) {
+    m2 <- LinkedMatrix(nrow = 15, ncol = 10, nNodes = 3, linkedBy = "rows",
+                       nodeInitializer = "ffNodeInitializer", vmode = "byte")
+    dim(m2)
+    nNodes(m2)
+    all(sapply(m2, inherits, "ff_matrix"))
+}
 
 # Create an empty 15x10 RowLinkedMatrix with 3 big.matrix nodes
-m3 <- LinkedMatrix(nrow = 15, ncol = 10, nNodes = 3, linkedBy = "rows",
-                   nodeInitializer = function(nodeIndex, nrow, ncol, ...) {
-                       bigmemory::big.matrix(nrow = nrow, ncol = ncol)
-                   })
-dim(m3)
-nNodes(m3)
-all(sapply(m3, class) == "big.matrix")
+if (require("bigmemory")) {
+    m3 <- LinkedMatrix(nrow = 15, ncol = 10, nNodes = 3, linkedBy = "rows",
+                       nodeInitializer = function(nodeIndex, nrow, ncol, ...) {
+                           bigmemory::big.matrix(nrow = nrow, ncol = ncol)
+                       })
+    dim(m3)
+    nNodes(m3)
+    all(sapply(m3, class) == "big.matrix")
+}
